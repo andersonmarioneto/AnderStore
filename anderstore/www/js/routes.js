b3758ed29876112d0389382e782d8fc8dcbd7a58
andersonmarioneto <netoanderson192@gmail.com>
2025-04-13 02:25:06 +0100

    update

--- anderstore/www/js/routes.js
+++ anderstore/www/js/routes.js
@@ -23,16 +23,44 @@ var app = new Framework7({
       animate: false,
 	  on: {
 		pageBeforeIn: function (event, page) {
-		// fazer algo antes da página ser exibida
+		  // fazer algo antes da página ser exibida
 		},
 		pageAfterIn: function (event, page) {
-		// fazer algo depois da página ser exibida
+		  // fazer algo depois da página ser exibida
 		},
 		pageInit: function (event, page) {
-		// fazer algo quando a página for inicializada
+		  // fazer algo quando a página for inicializada
+      $.getScript('js/index.js');
+
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        autoplay: true,
+        delay: 3000,
+        loop: true,
+        breakpoints: { 
+          50: {
+            slidesPerView: 1,
+            spaceBetween: 30
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          992: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 30
+          }
+        }
+      });
+
 		},
 		pageBeforeRemove: function (event, page) {
-		// fazer algo antes da página ser removida do DOM
+		  // fazer algo antes da página ser removida do DOM
 		},
 	  }
     },
